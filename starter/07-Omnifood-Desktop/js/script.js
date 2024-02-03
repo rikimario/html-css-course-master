2102302d70bd7da76a8a8c1b4e2387ff8dfa9a19
rikimario <mario_martinov@abv.bg>
2024-02-03 21:58:57 +0200

    implement sticky navigation

--- starter/07-Omnifood-Desktop/js/script.js
+++ starter/07-Omnifood-Desktop/js/script.js
@@ -38,6 +38,28 @@ allLinks.forEach(function (link) {
   });
 });
 
+// Sticky navigation
+const heroSectionEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entires) {
+    const ent = entires[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(heroSectionEl);
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
